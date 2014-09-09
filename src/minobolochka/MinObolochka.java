5bb6427e2f1bdbeab9ffb1969f3169fa418c11c3
Megawato <Megawato@Megawatto-PC>
2014-09-09 22:07:10 +0400

    Тест репозитория

--- src/minobolochka/MinObolochka.java
+++ src/minobolochka/MinObolochka.java
@@ -5,15 +5,11 @@ import java.util.ArrayList;
 public class MinObolochka {
 
     public static void main(String[] args) {
-//      Gui g1 = new Gui();
-//      g1.board.setPoint(new Randome().getPoints());
         Point[] p = new Randome().getPoints();
         Logic l = new Logic();
         l.solve(p);
         Gui g1 = new Gui();
         g1.board.setPoint(p, l.points2);
-//        Gui g2 = new Gui();
-//        g2.board.setPoint(l.points2, 1);
         System.out.println("__________");
 
         try {
